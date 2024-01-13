41391cef302490d9a65bbbbdd4b6ea769906c5dd
Minh Nguyen <1168534+minhnh@users.noreply.github.com>
2024-01-13 01:41:56 +0100

    add scene language, no dropping fluent
    - add minimal language to specify environments and agents
    - add DoesNotDropFluentClause as example of variant clause

--- src/bdd_textx/registration.py
+++ src/bdd_textx/registration.py
@@ -7,6 +7,16 @@ from bdd_textx.classes.bdd import UserStory, ScenarioTemplate
 CWD = abspath(dirname(__file__))
 
 
+def scene_metamodel():
+    mm_scene = metamodel_from_file(join(CWD, "grammars", "scene.tx"))
+    mm_scene.register_scope_providers(
+        {
+            "*.*": scoping_providers.FQNImportURI(),
+        }
+    )
+    return mm_scene
+
+
 def bdd_metamodel():
     mm_bdd = metamodel_from_file(
         join(CWD, "grammars", "bdd.tx"), classes=[UserStory, ScenarioTemplate]
@@ -19,6 +29,12 @@ def bdd_metamodel():
     return mm_bdd
 
 
+scene_lang = LanguageDesc(
+    "scene-tx",
+    pattern="*.scene",
+    description="Language for describing robotic scenes",
+    metamodel=scene_metamodel,
+)
 bdd_lang = LanguageDesc(
     "bdd-tx",
     pattern="*.bdd",
