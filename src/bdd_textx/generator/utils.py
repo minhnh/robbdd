1269180374589cb35ff7c35633f968ad4a01a807
Bastian Hunecke <bastianhunecke@gmail.com>
2024-08-25 16:17:47 +0200

    add working fluent generation

--- src/bdd_textx/generator/utils.py
+++ src/bdd_textx/generator/utils.py
@@ -1,6 +1,47 @@
-from pprint import pprint
 from rdf_utils.uri import URL_SECORO_M, URL_SECORO_MM
 from os import path
+
+def append_to_fluents(context, clause_type):
+    "Append a new fluent to the context if it does not exist"
+
+    if 'fluents' not in context:
+        context['fluents'] = []
+
+    if clause_type not in [fluent['clause_type'] for fluent in context['fluents']]:
+        fluent = {
+            'clause_type': clause_type
+        }
+        if clause_type == 'LocatedAtFluentClause':
+            fluent['name'] = 'located-at'
+            fluent['type'] = 'LocatedAtPredicate'
+        elif clause_type == 'DoesNotDropFluentClause':
+            fluent['name'] = 'does-not-drop'
+            fluent['type'] = 'DoesNotDropPredicate'
+        else:
+            fluent['name'] = clause_type
+            fluent['type'] = clause_type + 'Predicate'
+        context['fluents'].append(fluent)
+
+def create_clause_data(tc_type, clause_type):
+    "Create clause data based on time constraint type"
+
+    clause_data = {}
+    if tc_type == 'BeforeEvent':
+        clause_data['tc_name'] = 'before'
+        clause_data['ec_type'] = 'BeforeEventConstraint'
+    elif tc_type == 'AfterEvent':
+        clause_data['tc_name'] = 'after'
+        clause_data['ec_type'] = 'AfterEventConstraint'
+    elif tc_type == 'DuringEvent':
+        clause_data['tc_name'] = 'during'
+        clause_data['ec_type'] = 'DuringEventConstraint'
+    if clause_type == 'LocatedAtFluentClause':
+        clause_data['fl_name'] = 'located-at'
+    elif clause_type == 'DoesNotDropFluentClause':
+        clause_data['fl_name'] = 'does-not-drop'
+    else:
+        clause_data['fl_name'] = clause_type
+    return clause_data
 
 def prepare_context_data(metamodel, model):
     "Prepare and populate the context for jinja template"
@@ -41,30 +82,31 @@ def prepare_context_data(metamodel, model):
     context['namespaces']['scene'] = context['scene'].model.agent_ns[0].ns
     context['namespaces']['scene_env'] = context['scene'].model.environment_ns[0].ns
 
-    # Prepare fluents
-    context['fluents'] = []
-    for template in context['templates']:
-        for given_clause in template.given_clauses:
+    # Prepare fluents and clauses
+    context['fl_templates'] = []
+    for i in range(len(context['templates'])):
+        template = context['templates'][i]
+        context['fl_templates'].append({
+            'given_clauses': [],
+            'then_clauses': [],
+        })
+        for j in range(len(template.given_clauses)):
+            given_clause = context['templates'][i].given_clauses[j]
             clause_type = type(given_clause).__name__
-            if clause_type not in [fluent['clause_type'] for fluent in context['fluents']]:
-                fluent = {
-                    'clause_type': clause_type
-                }
-                if clause_type == 'LocatedAtFluentClause':
-                    fluent['name'] = 'fl-located-at'
-                    fluent['type'] = 'LocatedAtPredicate'
-                elif clause_type == 'DoesNotDropFluentClause':
-                    fluent['name'] = 'fl-does-not-drop'
-                    fluent['type'] = 'DoesNotDropPredicate'
-                else:
-                    fluent['name'] = 'fl-' + clause_type
-                    fluent['type'] = clause_type + 'Predicate'
-                context['fluents'].append(fluent)
+            append_to_fluents(context, clause_type)
             
-            # Prepare given clauses
-            
+            tc_type = type(given_clause.tc).__name__
+            fl_given_clause = create_clause_data(tc_type, clause_type)
+            context['fl_templates'][i]['given_clauses'].append(fl_given_clause)
 
-    pprint(type(context['templates'][0].given_clauses[0]).__name__)
+        for j in range(len(template.then_clauses)):
+            then_clause = context['templates'][i].then_clauses[j]
+            clause_type = type(then_clause).__name__
+            append_to_fluents(context, clause_type)
+
+            tc_type = type(then_clause.tc).__name__
+            fl_then_clause = create_clause_data(tc_type, clause_type)
+            context['fl_templates'][i]['then_clauses'].append(fl_then_clause)
 
     # Add behaviour filename to context
     file_name, _ = path.splitext(path.basename(model._tx_filename))
