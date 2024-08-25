de598d11970039fda58885cbf6c1435f2056e7fe
Bastian Hunecke <bastianhunecke@gmail.com>
2024-08-25 13:44:20 +0200

    update event clauses and added fluent generation

--- src/bdd_textx/generator/utils.py
+++ src/bdd_textx/generator/utils.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 from rdf_utils.uri import URL_SECORO_M, URL_SECORO_MM
+from os import path
 
 def prepare_context_data(metamodel, model):
     "Prepare and populate the context for jinja template"
@@ -37,11 +38,36 @@ def prepare_context_data(metamodel, model):
             context['namespaces']['environments'].name = ns_item.name.split("_")[0]
             context['environments_f'] = ns_item.name.split("_")[0]
 
-    print(context['scene'].model.agent_ns)
     context['namespaces']['scene'] = context['scene'].model.agent_ns[0].ns
     context['namespaces']['scene_env'] = context['scene'].model.environment_ns[0].ns
 
+    # Prepare fluents
+    context['fluents'] = []
+    for template in context['templates']:
+        for given_clause in template.given_clauses:
+            clause_type = type(given_clause).__name__
+            if clause_type not in [fluent['clause_type'] for fluent in context['fluents']]:
+                fluent = {
+                    'clause_type': clause_type
+                }
+                if clause_type == 'LocatedAtFluentClause':
+                    fluent['name'] = 'fl-located-at'
+                    fluent['type'] = 'LocatedAtPredicate'
+                elif clause_type == 'DoesNotDropFluentClause':
+                    fluent['name'] = 'fl-does-not-drop'
+                    fluent['type'] = 'DoesNotDropPredicate'
+                else:
+                    fluent['name'] = 'fl-' + clause_type
+                    fluent['type'] = clause_type + 'Predicate'
+                context['fluents'].append(fluent)
+            
+            # Prepare given clauses
+            
+
+    pprint(type(context['templates'][0].given_clauses[0]).__name__)
+
     # Add behaviour filename to context
-    context['behaviours_f'] = context['behaviours'][0].name
+    file_name, _ = path.splitext(path.basename(model._tx_filename))
+    context['behaviours_f'] = file_name
 
     return context
