f9b1f8795b7b0777e08b05d36ca4c5369a706735
Minh Nguyen <1168534+minhnh@users.noreply.github.com>
2026-06-29 17:23:49 +0200

    refactor scene model grammer
    - Move object and agent model bindings out of abstract scene elements to
      explicit declarations under ModelledScene.
    - Add link from ScenarioExecution to ModelledScene.
    - Adapt RDF generation: when generating from bddx model, only add linked
      ModelledScene to the graph.
    - Add optional ModelKind, support URDF, USD, MJCF for now.
    - Update models & tests.

--- src/robbdd/classes/bddx.py
+++ src/robbdd/classes/bddx.py
@@ -2,6 +2,7 @@
 from typing import Any
 from robbdd.classes.bdd import HoldsExpr, ScenarioVariant
 from robbdd.classes.common import IHasNamespaceDeclare
+from robbdd.classes.scene import ModelledScene
 
 
 class BehaviourImplementation(IHasNamespaceDeclare):
@@ -26,11 +27,13 @@ class ObservationPolicy(IHasNamespaceDeclare):
 
 class ScenarioExecution(IHasNamespaceDeclare):
     variant: ScenarioVariant
+    modelled_scene: ModelledScene
     bhv_impl: BehaviourImplementation
     obs_policies: list[ObservationPolicy]
 
-    def __init__(self, parent, ns, name, variant, bhv_impl, obs_policies) -> None:
+    def __init__(self, parent, ns, name, variant, modelled_scene, bhv_impl, obs_policies) -> None:
         super().__init__(parent=parent, ns=ns, name=name)
         self.variant = variant
+        self.modelled_scene = modelled_scene
         self.bhv_impl = bhv_impl
         self.obs_policies = obs_policies
